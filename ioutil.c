fd28c98da2c72ef4c66f199a66de2b81338199e5
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-13 21:13:13 +0300

    Update ioutil.c

--- ioutil.c
+++ ioutil.c
@@ -1,1 +1,19 @@
 
+int __strlen(const char *s) {
+    int i = 0;
+    while (s[i]) i++;
+    return i;
+}
+
+bool __isdigit(char d) {
+    return '0' <= d && d <= '9';
+}
+
+bool __isspace(char s) {
+    static const char spaces[] = " \t\n\v\f\r";
+    int i = 0;
+    for (i = 0; i < sizeof spaces; i++)
+        if (spaces[i] == s)
+            return true;
+    return false;
+}
