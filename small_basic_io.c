28d57cac880b4e702be3a22d9c58a68405603baa
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-13 21:07:45 +0300

    Update small_basic_io.c

--- small_basic_io.c
+++ small_basic_io.c
@@ -6,54 +6,19 @@
 
 #include "std.h"
 
-#define __getchar getchar
-#define __putchar putchar
-#define __isspace isspace
-
-
-int __strlen(const char *s) {
-    int i = 0;
-    while (s[i]) i++;
-    return i;
-}
-
-bool __isdigit(char d) {
-    return '0' <= d && d <= '9';
-}
-
-void __readint(int *x) {
-    char c = 0;
-    while (c == '\b' || !__isspace(c)) {
-        c = __getchar();
-        if (__isdigit(c))
-            (*x) = (*x) * 10 + c - '0';
-    }
-}
-
-int __printf(const char *fmt, ...) {
+int __sprintf(char *dst, const char *fmt, ...) {
     int i = 0;
     va_list args;
     va_start(args, fmt);
-    for (; i < __strlen(fmt); i++) {
-        if (fmt[i] != '%')
-            __putchar(fmt[i]);
-        else {
-            if (fmt[i + 1] == '%') /* will not left the string, because if i == n - 1, then fmt[i + 1] == 0 */
-                __putchar('%');
-            else {
-                switch (fmt[i]) {
-                    // va_arg(args, int);
-                }
-            }
-        }
-    }
+    // va_arg(args, int);
     va_end(args);
     return 0;
 }
 
-int __scanf(const char *fmt, ...) {
+int __sscanf(const char *src, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
+
     va_end(args);
     return 0;
 }
