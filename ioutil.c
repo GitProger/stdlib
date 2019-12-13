e55de786e87b39e44a0150bb79a2af49c42ff2a6
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-13 21:49:33 +0300

    Update ioutil.c

--- ioutil.c
+++ ioutil.c
@@ -1,3 +1,6 @@
+/**
+ *  functions from <ctype.h> + strlen
+ */
 
 int __strlen(const char *s) {
     int i = 0;
@@ -5,25 +8,64 @@ int __strlen(const char *s) {
     return i;
 }
 
-bool __isdigit(char d) {
-    return '0' <= d && d <= '9';
+bool __isdigit(char c) {
+    return '0' <= c && c <= '9';
 }
 
-bool __isspace(char s) {
+bool __isxdigit(char c) {
+    return __isdigit(c) || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F');
+}
+
+bool __isspace(char c) {
     static const char spaces[] = " \t\n\v\f\r";
     int i = 0;
     for (i = 0; i < sizeof spaces; i++)
-        if (spaces[i] == s)
+        if (spaces[i] == c)
+            return true;
+    return false;
+}
+
+bool __isgraph(char c) {
+    return !__isspace(c);
+}
+
+bool __islower(char c) {
+    return ('a' <= x && x <= 'z');
+}
+
+bool __isupper(char c) {
+    return ('A' <= x && x <= 'Z');
+}
+
+bool __isalpha(char c) {
+    return __islower(c) || __isupper(c);
+}
+
+bool __isalnum(char c) {
+    return __isaplha(c) || __isdigit(c);
+}
+
+bool __ispunct(char c) {
+    static const char pun[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    int i = 0;
+    for (i = 0; i < sizeof pun; i++)
+        if (pun[i] == c)
             return true;
     return false;
 }
 
 char __toupper(char x) {
-    if ('a' <= x && x <= 'z') x ^= 0x20;
+    if (__islower(x)) x ^= 0x20;
     return x;
 }
 
 char __tolower(char x) {
-    if ('A' <= x && x <= 'Z') x ^= 0x20;
+    if (__isupper(x)) x ^= 0x20;
     return x;
 }
+
+/*
+int isblank(int c);
+int iscntrl(int c);
+int isprint(int c);
+*/
